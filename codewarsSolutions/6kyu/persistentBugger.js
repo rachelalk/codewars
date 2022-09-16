c250090d21c59c46cb4882a3d7cde2639ea9dc18
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-09-16 14:45:06 +0100

    persistent bugger

---
+++ codewarsSolutions/6kyu/persistentBugger.js
@@ -0,0 +1,36 @@
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example (Input --> Output):
+
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// 4 --> 0 (because 4 is already a one-digit number)
+
+function reducer(theNumArray) {
+	var i,
+		sum = 1;
+	for (i = 0; i < theNumArray.length; i++) {
+		sum *= theNumArray[i];
+	}
+	return sum;
+}
+
+function persistence(num) {
+	if (num.length === 1) {
+		return 0;
+	}
+	let count = 0;
+	let result = num;
+	console.log(result);
+	while (result > 9) {
+		let splitNum = result.toString().split("");
+		console.log(splitNum);
+		let digits = splitNum.map(Number);
+		console.log(digits);
+		result = reducer(digits);
+		console.log(result);
+		count++;
+		console.log(count);
+	}
+	return count;
+}
