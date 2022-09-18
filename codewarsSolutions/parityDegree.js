fd1eaf7598b9640e9c12a66c4786c200539c5327
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-09-18 19:15:20 +0100

    parity degree

---
+++ codewarsSolutions/parityDegree.js
@@ -0,0 +1,27 @@
+// A positive integer N is given. The goal is to find the highest power of 2 that divides N. In other words, we have to find the maximum K for which N modulo 2^K is 0.
+
+// For example, given integer N = 24 the answer is 3, because 2^3 = 8 is the highest power of 2 that divides N.
+
+// Write a function:
+
+// function solution(N);
+
+// that, given a positive integer N, returns the highest power of 2 that divides N.
+
+// For example, given integer N = 24, the function should return 3, as explained above.
+
+// Assume that:
+
+// N is an integer within the range [1..1,000,000,000].
+
+
+function solution(N) {
+	let array = [];
+	for (let i = 0; i < N; i++) {
+		let digit = Math.pow(2, i);
+		if (N % digit === 0) {
+			array.push(i);
+		}
+	}
+	return array[array.length - 1];
+}
